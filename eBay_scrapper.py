6fff14bf0b8ae79beb38d9db5e4aaa8237b3eddf
Kuan <kuan0019@umn.edu>
2018-11-10 17:49:13 -0600

    c

--- eBay_scrapper.py
+++ eBay_scrapper.py
@@ -5,16 +5,16 @@ import pickle
 uClient = urllib.request.urlopen("https://www.ebay.com/v/allcategories")
 content = uClient.read()
 uClient.close()
-count = 0
+
 categories = {}
 soup = BeautifulSoup(content, "html.parser")
 sub_categories = soup.findAll("li",{"class":"sub-category"})
 for i in sub_categories:
-    count += 1
     sub_category = (i.text.split("-")[0]).strip()
     main_category = (i.text.split("-")[1]).strip()
     if (main_category not in categories.keys()):
         categories[main_category] = []
     categories[main_category].append(sub_category)
 
-print(count)
+print(categories)
+pickle.dump(categories, open("eBay_categories.pkl","wb"))
